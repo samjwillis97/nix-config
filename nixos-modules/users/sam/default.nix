01e3e778ac264f593d4c46c18b77327f5c636978
samjwillis97 <sam@williscloud.org>
2026-08-22 13:12:27 +1000

    Removing unused user files

--- nixos-modules/users/sam/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  isNormalUser = true;
-  shell = pkgs.bash;
-  # openssh.authorizedKeys.keys = lib.mkForce (builtins.readFile ./users/${user}/ssh/authorized_keys);
-  initialPassword = "nixos";
-  extraGroups = [ "wheel" ];
-}
