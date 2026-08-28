6ec9cd0998a4cba3c751d39b342a4dc53d5c338c
samjwillis97 <sam@williscloud.org>
2026-08-28 17:08:30 +1000

    Adding insomnia + dbeaver

--- home-modules/development.nix
+++ home-modules/development.nix
@@ -3,42 +3,54 @@
 , pkgs
 , ...
 }:
+let
+  desktopEnabled = config.my.desktop.enable;
+in
 {
   options.my.development = {
     enable = lib.mkEnableOption "development environment";
   };
 
-  config = lib.mkIf config.my.development.enable {
-    my.f.enable = true;
+  config = lib.mkIf config.my.development.enable lib.mkMerge [
+    {
+      my.f.enable = true;
 
-    home.packages = with pkgs; [
-      neovim
+      home.packages = with pkgs; [
+        neovim
 
-      # JSON Tooling
-      jq
-      jless
-    ];
+        # JSON Tooling
+        jq
+        jless
 
-    home.sessionVariables = {
-      EDITOR = pkgs.lib.getExe pkgs.neovim;
-    };
+      ];
 
-    programs = {
-      direnv = {
-        enable = true;
-        enableZshIntegration = true;
-        nix-direnv.enable = true;
-        config = {
-          hide_env_diff = true;
-          warn_timeout = "30s"; # Reduce timeout overhead
+      home.sessionVariables = {
+        EDITOR = pkgs.lib.getExe pkgs.neovim;
+      };
+
+      programs = {
+        direnv = {
+          enable = true;
+          enableZshIntegration = true;
+          nix-direnv.enable = true;
+          config = {
+            hide_env_diff = true;
+            warn_timeout = "30s"; # Reduce timeout overhead
+          };
+        };
+
+        direnv-instant = {
+          enable = true;
+          enableZshIntegration = true;
         };
       };
+    }
 
-      direnv-instant = {
-        enable = true;
-        enableZshIntegration = true;
-      };
-    };
-
-  };
+    (lib.mkIf desktopEnabled {
+      home.packages = with pkgs; [
+        insomnia
+        dbeaver-bin
+      ];
+    })
+  ];
 }
