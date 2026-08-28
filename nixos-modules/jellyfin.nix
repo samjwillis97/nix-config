94dc41c90c0096947ebb1719c88ca7f4dc459759
samjwillis97 <sam@williscloud.org>
2026-08-28 21:46:13 +1000

    WIP - dispatcahrr -> jellyfin

---
+++ nixos-modules/jellyfin.nix
@@ -0,0 +1,19 @@
+{ config, lib, ... }:
+{
+  options.my.jellyfin = {
+    enable = lib.mkEnableOption "jellyfin";
+
+    ingress.enable = lib.mkEnableOption "jellyfin ingress route";
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf config.my.jellyfin.enable { })
+
+    (lib.mkIf (config.my.jellyfin.enable && config.my.actual.ingress.enable) {
+      my.ingress.routes.jellyfin = {
+        upstream = "http://127.0.0.1:${toString config.services.jellyfin.settings.port}";
+        websockets = true;
+      };
+    })
+  ];
+}
--- packages/dispatcharr/README.md
+++
@@ -1,3 +0,0 @@
-# Dispatacharr
-
-Was copied from https://github.com/typedrat/nix-config/blob/master/packages/dispatcharr/package.nix
