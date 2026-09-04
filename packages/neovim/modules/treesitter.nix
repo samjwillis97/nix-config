14bb4f8f4d4fcb6eea33251438814eac745f2101
samjwillis97 <sam@williscloud.org>
2026-09-04 20:56:30 +1000

    Adding treesitter context

--- packages/neovim/modules/treesitter.nix
+++ packages/neovim/modules/treesitter.nix
@@ -1,19 +1,65 @@
 { config, lib, ... }:
 {
-  plugins.treesitter = {
-    enable = true;
-
-    # Load when reading a buffer for syntax highlighting
-    lazyLoad.settings = lib.mkIf config.my.lazyLoading.enable {
-      event = "BufReadPost";
+  options.my.treesitter = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable treesitter support";
     };
 
-    nixvimInjections = true;
-    nixGrammars = true;
-
-    settings = {
-      highlight.enable = true;
-      indent.enable = true;
+    showContext = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Show treesitter context";
     };
   };
+
+  config = lib.mkIf config.my.treesitter.enable (
+    lib.mkMerge [
+      {
+        plugins.treesitter = {
+          enable = true;
+
+          # Load when reading a buffer for syntax highlighting
+          lazyLoad.settings = lib.mkIf config.my.lazyLoading.enable {
+            event = "BufReadPost";
+          };
+
+          nixvimInjections = true;
+          nixGrammars = true;
+
+          settings = {
+            highlight.enable = true;
+            indent.enable = true;
+          };
+        };
+      }
+
+      (lib.mkIf config.my.treesitter.showContext {
+        highlightOverride = {
+          "TreesitterContextBottom" = {
+            underline = false;
+          };
+          "TreesitterContextLineNumberBottom" = {
+            underline = false;
+          };
+        };
+
+        plugins.treesitter-context = {
+          enable = true;
+
+          # Load after buffer is read (after treesitter)
+          lazyLoad.settings = lib.mkIf config.my.lazyLoading.enable {
+            event = "BufReadPost";
+          };
+
+          settings = {
+            line_numbers = true;
+            max_lines = 10;
+            multiline_threshold = 5;
+          };
+        };
+      })
+    ]
+  );
 }
