02262aa1d3d9bd6eaeb5b8a754025da4d8a81cab
samjwillis97 <sam@williscloud.org>
2026-09-07 22:14:21 +1000

    fixing file path in mini statusline

--- packages/neovim/modules/statusline.nix
+++ packages/neovim/modules/statusline.nix
@@ -22,6 +22,49 @@
 
         settings = {
           use_icons = config.my.icons.enable;
+
+          content = {
+            active.__raw = ''
+              function()
+                local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+                local git           = MiniStatusline.section_git({ trunc_width = 40 })
+                local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+                local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+                local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+
+                local filename = vim.fn.fnamemodify(
+                  vim.api.nvim_buf_get_name(0),
+                  ':.'
+                ) .. '%m%r'
+
+                local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+                local location = MiniStatusline.section_location({ trunc_width = 75 })
+
+                return MiniStatusline.combine_groups({
+                  { hl = mode_hl,                 strings = { mode } },
+                  { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+                  '%<',
+                  { hl = 'MiniStatuslineFilename', strings = { filename } },
+                  '%=',
+                  { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                  { hl = mode_hl,                 strings = { location } },
+                })
+              end
+            '';
+
+            inactive.__raw = ''
+              function()
+                local filename = vim.fn.fnamemodify(
+                  vim.api.nvim_buf_get_name(0),
+                  ':.'
+                ) .. '%m%r'
+
+                return MiniStatusline.combine_groups({
+                  { hl = 'MiniStatuslineFilename', strings = { filename } },
+                })
+              end
+            '';
+          };
         };
       };
     })
