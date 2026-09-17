614e71efd05c8fcbb9318571758a27a7eae329f6
samjwillis97 <sam@williscloud.org>
2026-09-17 19:24:36 +1000

    More config

--- home-modules/desktop.nix
+++ home-modules/desktop.nix
@@ -19,6 +19,10 @@
         # auto mounting of external storage devices
         services.udiskie.enable = true;
 
+        home.packages = with pkgs; [
+          wmenu
+        ];
+
         wayland.windowManager.sway = {
           enable = true;
           wrapperFeatures.gtk = true;
@@ -27,9 +31,16 @@
           # required false for swayfx
           checkConfig = false;
 
-          config = rec {
+          config = {
             modifier = "Mod4";
+
+            up = "k";
+            down = "j";
+            left = "h";
+            right = "l";
+
             terminal = "ghostty";
+            menu = "wmenu";
 
             workspaceAutoBackAndForth = true;
             workspaceLayout = "default";
@@ -38,14 +49,40 @@
               followMouse = false;
             };
 
-            keybindings = {
-              "${modifier}+Return" = "exec ${terminal}";
-              "${modifier}+Shift+q" = "kill";
+            keybindings =
+              let
+                modifier = config.wayland.windowManager.sway.config.modifier;
+                workspaces = [
+                  1
+                  2
+                  3
+                  4
+                  5
+                  6
+                  7
+                  8
+                  9
+                  0
+                ];
+              in
+              {
+                "${modifier}+Return" = "exec $terminal";
+                "${modifier}+Shift+q" = "kill";
 
-              "${modifier}+n" = "exec ${lib.getExe config.programs.firefox.package}";
-              "${modifier}+d" = "exec ${lib.getExe pkgs.wmenu}";
-            };
+                "${modifier}+n" = "exec ${lib.getExe config.programs.firefox.package}";
+                "${modifier}+d" = "exec $menu";
+              };
           };
+
+          gaps = {
+            inner = 15;
+            outer = 15;
+          };
+
+          extraSessionCommands = ''
+            # give Sway a little time to startup before starting kanshi.
+            exec sleep 5; systemctl --user start kanshi.service
+          '';
 
           extraConfig = ''
             shadows enable
