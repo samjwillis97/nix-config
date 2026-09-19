7ee73f0f9c31c25faed4598fe3f271c78a80d365
samjwillis97 <sam@williscloud.org>
2026-09-19 12:31:54 +1000

    More desktop + gaming + sway stuff

--- flake-module.nix
+++ flake-module.nix
@@ -84,6 +84,7 @@ let
       sharedHostHomeModule = {
         config.my.desktop.enable = lib.mkForce config.my.desktop.enable;
         config.my.work.enable = lib.mkForce config.my.work.enable;
+        config.my.gaming.enable = lib.mkForce config.my.gaming.enable;
       };
     in
     {
