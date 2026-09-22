204958ece8f3cc802e462f9b614889eb6435c4c4
samjwillis97 <sam@williscloud.org>
2026-09-23 09:05:04 +1000

    Adding perosnal-desktop to the github deployment list

--- nixos-hosts/personal-desktop/default.nix
+++ nixos-hosts/personal-desktop/default.nix
@@ -30,6 +30,7 @@
 
       deploy-rs = {
         enable = true;
+        githubActions.enable = true;
         authorizedKeys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2FeFN6YQEUr22lJCeuQHcDawLuAPnoizlZLJOwhch4 sam@williscloud.org"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYyMM/qTTLsXdPvvfkhdufg9gLYOI2y8d1oDpAgI0ft samjwillis97@gmail.com"
